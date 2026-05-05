457323d32539b651f1d4e050c1209d3372fd18f2
gavsteve@iu.edu <gavsteve@iu.edu>
2026-05-05 10:07:22 -0400

    Query6

--- Project_sql/6_Advanced_SQL.sql
+++ Project_sql/6_Advanced_SQL.sql
@@ -5,7 +5,12 @@ in which I use the average yearly salary to replace the null values.
 
 SELECT 
     job_title_short,
-    COALESCE(salary_year_avg, ROUND((SELECT avg(salary_year_avg) FROM job_postings_fact)), 2) as salary_estimate_2
+    COALESCE(salary_year_avg, ROUND((SELECT avg(salary_year_avg) FROM job_postings_fact)), 2) as salary_estimate_2,
+    COALESCE(salary_year_avg, ROUND((LAG(COALESCE(salary_year_avg, ROUND((SELECT avg(salary_year_avg) FROM job_postings_fact)), 2)) OVER() + LEAD(COALESCE(salary_year_avg, ROUND((SELECT avg(salary_year_avg) FROM job_postings_fact)), 2)) OVER()) / 2), 2) AS salary_estimate3,
+    ROW_NUMBER() OVER() as row_num,
+    LAG(COALESCE(salary_year_avg, ROUND((SELECT avg(salary_year_avg) FROM job_postings_fact)), 2)) OVER() as prior_value, 
+    LEAD(COALESCE(salary_year_avg, ROUND((SELECT avg(salary_year_avg) FROM job_postings_fact)), 2)) OVER() as lead_value 
 FROM job_postings_fact
 WHERE
-    job_title_short like '%Data Analyst%'
+    job_title_short like '%Data Analyst%';
+
