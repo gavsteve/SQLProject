64f550e872891c3bfcf9825ccd2df4ff7ce78847
gavsteve@iu.edu <gavsteve@iu.edu>
2026-05-05 09:54:32 -0400

    Query 6

---
+++ Project_sql/6_Advanced_SQL.sql
@@ -0,0 +1,11 @@
+/*
+Learning how to use the COALESCE concept in SQL. More than that, I am learning to use the amputation technique
+in which I use the average yearly salary to replace the null values. 
+*/
+
+SELECT 
+    job_title_short,
+    COALESCE(salary_year_avg, ROUND((SELECT avg(salary_year_avg) FROM job_postings_fact)), 2) as salary_estimate_2
+FROM job_postings_fact
+WHERE
+    job_title_short like '%Data Analyst%'
